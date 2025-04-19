e708670501075e545ec80197648d878fdc57ce6b
Orinson <dojcn123@gmail.com>
2025-04-18 20:19:30 -0500

    version 0.3.1

--- scripts.js
+++ scripts.js
@@ -2,18 +2,18 @@ const cards= document.getElementById('cards');
 const errorSection = document.getElementById('error-section');
 let todosLosPokemons = [];
 
-async function cargarPokemons() {
+async function cargarPokemons() { //Funcion principal de carga de pokemons
   cards.innerHTML="";
 
   try {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=410');
-    if (!res.ok) throw new Error(`Error en la solicitud: ${res.status}`);
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=410'); //Peticion HTTP GET. Guardamos la respuesta de la API en la variable res.
+    if (!res.ok) throw new Error(`Error en la solicitud: ${res.status}`); //Revisamon si fue exitosa la peticion, si no lanzamos error.
 
-    const data= await res.json();
-    const detallesPromises = data.results.map( pokemon =>
-      fetch(pokemon.url).then(r => r.json())
-    );
-    todosLosPokemons = data.results;
+    const data= await res.json(); //Convertimos la respuesta a un objeto JavaScript. data representara toda la respuesta de la API.
+    const detallesPromises = data.results.map( pokemon => //data.results.map() Recorre el array de objetos pokemon y crea uno nuevo con lo que le devolvés en cada iteración. Lo que entrega se lo entrega como parámetro a la funcion ()
+      fetch(pokemon.url).then(r => r.json()) //Usamos fetch otra vez para obtener todos los detalles los pokemon, (los cuales estan en la url que está en el objeto que se entrego con el fetch original), cada uno por cada uno especificamente. 
+    ); //Then toma la respuesta del fetch, y con una funcion flecha lo combierte en objeto JS, el objeto response transformado a JSON es r.
+    todosLosPokemons = data.results; //Guardamos el array de pokemons obtenidos desde la API, los cuales tenemos gracias al res.josn(). 410 pokemon
     cargarRangoPokemons(1,15); 
 
 
@@ -25,26 +25,28 @@ async function cargarPokemons() {
 }
 
 async function cargarRangoPokemons(from,to){
-  cards.innerHTML= "";
-  const pokemonsAMostrar = todosLosPokemons.slice(from - 1, to);
+  cards.innerHTML= ""; //Limpiamos el contenido actual de las cards para no acumularlas
+  const pokemonsAMostrar = todosLosPokemons.slice(from - 1, to); //.slice nos permite tomar una parte del array. En este caso desde la posicion from - 1 hasta to (valores ya preestablecidos). from - 1 porque los arrays comienzan en 0.
 
-  const informacion = await Promise.all(
-    pokemonsAMostrar.map(p => fetch(p.url).then(r => r.json()))
+  const informacion = await Promise.all( //Esperá a que todas las peticiones se resuelvan, y después guardá la info completa de todos los Pokémon en informacion
+    pokemonsAMostrar.map(p => fetch(p.url).then(r => r.json())) //De los pokemon a mostar, recorre el array y transforma cada objeto en otra cosa... Del fetch obtienes la info en bruto, y la pasas al .then que la trans forma a un objeto JS.
   );
 
-  informacion.forEach(pokemon => {
-    const tipos = pokemon.types.map(t =>
-      `<div class="${t.type.name}"><p>${t.type.name.toUpperCase()}</p></div>`
+  informacion.forEach(pokemon => { //recorre uno por uno y te da cada Pokémon como parámetro en la variable.
+    const tipos = pokemon.types.map(t => //pokemon.types es un array que contiene los tipos que puede tener un pokemon. Con map vamos a recorrerlos.
+      `<div class="${t.type.name}"><p>${t.type.name.toUpperCase()}</p></div>` // t es un de los tipos del pokemon t.type selecciona algo como {name: 'grass'} y t.type.name selecciona 'grass'
     ).join("");
 
     const habilidades = pokemon.abilities.map(h =>
       `<li>${h.ability.name}</li>`
     ).join("");
-
-    cards.innerHTML += `
+    //Creamos las tarjetas desde aquí con HTML
+    //A cards le agregamos ...
+    //todo eso son propiedades que ya tenemos
+    cards.innerHTML += ` 
       <div class="pokemons-cards">
         <div class="image">
-          <img width="170px" src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+          <img width="170px" src="${pokemon.sprites.front_default}" alt="${pokemon.name}"> 
           <h1>${String(pokemon.id).padStart(3, '0')}</h1>
         </div>
         <div class="pokdesc">
@@ -68,7 +70,7 @@ async function cargarRangoPokemons(from,to){
 
 }
 
-
+//esto se activa al presionar el botón, lo que ejecuta todo el código anteriormente visto dándole funcionalidad a la página.
 window.addEventListener("DOMContentLoaded", cargarPokemons);
 
 
